d0d014d2856d673968d0276b7bf981624066c713
Jordan <illogicalTree@gmail.com>
2021-10-02 22:32:17 +0100

    adds super basic product page

---
+++ frontend/src/components/Product.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="product">
+    <h1>{{product.title}}</h1>
+    <h2>{{product.price}}</h2>
+    <h3>{{product.description}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['product'],
+};
+</script>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,23 @@
 import { createStore } from 'vuex';
 
+const cloudantUrl = 'https://awesome-product-api.eu-gb.mybluemix.net';
+
 export default createStore({
   state: {
+    products: [],
   },
   mutations: {
+    setProducts(state, products) {
+      state.products = products;
+    },
   },
   actions: {
+    getProducts({ commit }) {
+      fetch(`${cloudantUrl}/products`)
+        .then((response) => response.json())
+        .then((products) => commit('setProducts', products))
+        .catch((error) => console.log(error));
+    },
   },
   modules: {
   },
